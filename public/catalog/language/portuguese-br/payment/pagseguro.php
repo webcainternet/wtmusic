36520cdb861c979753deaa7268f4b119f9d60519
fernandofmendes <fernando.mendes@webca.com.br>
2016-02-03 00:29:42 -0200

    text cartao

--- public/catalog/language/portuguese-br/payment/pagseguro.php
+++ public/catalog/language/portuguese-br/payment/pagseguro.php
@@ -1,5 +1,7 @@
 <?php
-$_['text_title'] 				= 'PagSeguro';
-$_['button_confirm_pagseguro'] 	= 'Confirmar e ir para o PagSeguro';
-$_['text_extra_amount']			= 'Frete e Taxas';
+$_['text_title'] 		= 'PagSeguro - Cartão de Débito, Crédito ou Boleto';
+$_['text_information'] 	= 'Ao clicar no botão abaixo, você será redirecionado para o site do PagSeguro para efeturar o pagamento. Ao concluir o pagamento você retornará automaticamente para a loja.';
+$_['text_wait']         = 'Por favor, aguarde!';
+$_['button_confirm_pagseguro'] = 'Confirmar e ir para o PagSeguro';
+$_['text_extra_amount']	= 'Frete e Taxas';
 ?>
